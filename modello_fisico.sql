cb8c5134d96175a44952261806b3e121c6321246
Krobi <krobi@cock.li>
2020-05-29 15:44:09 +0200

    sono stati sistemati degli errori con le serie, che erano legate unicamente a un esercizio e non alla coppia esercizio allenamento

--- modello_fisico.sql
+++ modello_fisico.sql
@@ -41,37 +41,56 @@ create table esercizio (
     id serial primary key,
     nome text not null,
     descrizione text,
-    gruppo_muscolare gruppi_muscolari
 );
 
-create table composto (
-    allenamento_id serial references allenamento(id),
+create table gruppo_muscolare (
+    id serial primary key,
+    nome gruppi_muscolari unique
+);
+
+create table esercizio_gruppo (
     esercizio_id serial references esercizio(id),
-    primary key (allenamento_id, esercizio_id)
+    gruppo_muscolare_id serial references gruppo_muscolare(id),
+    primary key (esercizio_id, gruppo_muscolare_id)
 );
 
 create table esercizio_serie (
-    esercizio_id serial primary key references esercizio(id),
+    id serial primary key,
     numero smallint not null,
-    intervallo interval,
+    intervallo interval not null,
     durata interval,
     nome_strumento_utilizzato text,
-    peso_strumento real
+    peso_strumento real,
+    unique(numero, intervallo)
+);
+
+create table composizione_serie (
+    allenamento_id serial references allenamento(id),
+    esercizio_id serial references esercizio(id),
+    esercizio_serie_id serial references esercizio_serie(id),
+    primary key (allenamento_id, esercizio_id, esercizio_serie_id)
 );
 
 create table dati_esercizi_serie (
-    esercizio_serie_id serial primary key references esercizio_serie(esercizio_id),
+    esercizio_serie_id serial primary key references esercizio_serie(id),
     carico real not null,
     serie_corrente smallint not null unique
 );
 
 create table riscaldamento (
-    esercizio_id serial primary key references esercizio(id),
+    id serial primary key,
     durata interval not null
 );
 
+create table composizione_riscaldamento (
+    allenamento_id serial references allenamento(id),
+    esercizio_id serial references esercizio(id),
+    riscaldamento_id serial references riscaldamento(id),
+    primary key (allenamento_id, esercizio_id, riscaldamento_id)
+);
+
 create table ripetizione (
-    esercizio_serie_id serial primary key references esercizio_serie(esercizio_id),
+    esercizio_serie_id serial primary key references esercizio_serie(id),
     numero smallint not null
 );
 
